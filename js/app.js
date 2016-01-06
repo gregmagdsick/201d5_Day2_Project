0d36446313a0eb9cc923406e2aa632dc06f395f9
Greg Magdsick <greg.magdsick@gmail.com>
2016-01-06 12:57:18 -0800

    added counter for keeping score

--- js/app.js
+++ js/app.js
@@ -7,10 +7,16 @@ alert('Hi ' + user + '. I\'m going to ask you 3 yes or no questions. Please answ
 var answer1 = prompt(user + ', am I a resident of Washington state?');
 console.log('user\'s answer1 is: ' + answer1);
 
-// make is so we can accept 'y' or 'Y' or 'yes' or 'YES' as an acceptable response to the question//
+// setting up counter for keeping score of how many correct answers the user gets
+var counter = 0
+
+
+// make is so we can accept 'y' or 'Y' or 'yes' or 'YES' as an acceptable response to the question
 if (answer1.toLowerCase() === 'y' || answer1.toLowerCase() === 'yes') {
   alert('Great ' + user + ', you got the first question right')
   console.log('user got question1 correct.');
+  counter ++;
+  console.log('variable counter now = ' + counter);
 } else {
   alert('Sorry ' + user + ', that\'s not right, I am a resident of Washington.')
   console.log('user got question 1 wrong.');
@@ -18,10 +24,12 @@ if (answer1.toLowerCase() === 'y' || answer1.toLowerCase() === 'yes') {
 
 var answer2 = prompt(user + ', here is question number 2: Do I have brown hair?');
 console.log('user\'s answer2 is: ' + answer2);
-// make is so we can accept 'y' or 'Y' or 'yes' or 'YES' as an acceptable response to the question//
+// make is so we can accept 'y' or 'Y' or 'yes' or 'YES' as an acceptable response to the question
 if (answer2.toLowerCase() === 'y' || answer2.toLowerCase() === 'yes') {
   alert('Fantastic ' + user + ', you are right!')
   console.log('user got question2 correct');
+  counter ++;
+  console.log('variable counter now = ' + counter);
 } else {
   alert('Bummer ' + user + ', you got question 2 wrong.')
   console.log('user got question2 wrong. I do have brown hair.');
@@ -32,6 +40,8 @@ console.log('user\'s answer3 is: ' + answer3);
 if (answer3.toLowerCase() === 'n' || answer3.toLowerCase() === 'no') {
   alert('Awesome ' + user + ', you got this one right!');
   console.log('user got question3 right.');
+  counter ++;
+  console.log('variable counter now = ' + counter);
 } else {
   alert('Oops, ' + user + ', you got this one wrong.');
   console.log('user got question3 wrong.');
